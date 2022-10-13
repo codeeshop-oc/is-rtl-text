968beb276b2168e20e678668f6554f6f22b6d838
Anant <anantnegi8@gmail.com>
2022-10-13 14:19:15 +0530

    test file updated

--- test/index.test.js
+++ test/index.test.js
@@ -10,14 +10,14 @@ describe('index', function () {
     test('test check number 4', function () {
         expect(isRTL(4)).toBeFalsy();
     });
-    test('test Urdu کھ', function () {
-        expect(isRTL('کھ')).toBe(true);
+    test('test Urduہیلو', function () {
+        expect(isRTL('ہیلو')).toBe(true);
     });
-    test('test Mixed #correct w مهسا_امینی', function () {
-        expect(isRTL('#correct w مهسا_امینی')).toEqual(true);
+    test('test Mixed #correct خوش آمدید', function () {
+        expect(isRTL('#correct خوش آمدید')).toEqual(true);
     });
-    test('test sure RTL العربية', function () {
-        expect(isRTL('العربية')).toBeTruthy();
+    test('test sure RTL اب تیار ہے', function () {
+        expect(isRTL('اب تیار ہے')).toBeTruthy();
     });
     test('test sure not RTL Test', function () {
         expect(isRTL('Test')).toBe(false);
